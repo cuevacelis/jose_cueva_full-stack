3769083f9835582ab3b1ae4825a99d4fd220e056
cuevacelis <cuevacelis@hotmail.com>
2025-09-30 14:50:58 -0500

    feat: Add authentication header components and theme mode, and improve navigation

--- src/app/(auth)/dashboard/_layout/auth-header.tsx
+++ src/app/(auth)/dashboard/_layout/auth-header.tsx
@@ -1,11 +1,18 @@
 "use client";
 
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { logoutAction } from "@/app/(auth)/dashboard/_actions/logout-action";
 import { Separator } from "@/components/ui/separator";
+import { LogOut } from "lucide-react";
+import LogoMobile from "../_components/ui/logo-mobile";
+import { ThemeModeToggle } from "@/components/theme-toggle/theme-mode-toggle";
+import { cn } from "@/lib/utils/class-utils";
 
 export function AuthHeader() {
+  const pathname = usePathname();
+
   const handleLogout = async () => {
     await logoutAction();
   };
@@ -14,20 +21,31 @@ export function AuthHeader() {
     <header className="font-montserrat text-base flex items-center justify-between px-6 py-6 md:px-10 lg:px-20">
       <Link
         href="/dashboard"
-        className="text-xl lg:text-2xl font-bold text-primary hover:text-foreground transition-colors"
+        className="text-xl lg:text-2xl font-bold text-primary"
       >
-        ♪ MUSIC
+        <LogoMobile className="text-primary inline md:hidden" />
+        <span className="text-primary hidden md:inline">♪ MUSIC</span>
       </Link>
-      <div className="flex justify-center items-center gap-10">
+      <div className="flex justify-center items-center gap-2.5 lg:gap-10">
         <Link
           href="/dashboard"
-          className="text-base font-semibold transition-colors hover:text-primary"
+          className={cn(
+            "text-base font-semibold transition-colors hover:text-primary",
+            {
+              "text-primary": pathname === "/dashboard",
+            }
+          )}
         >
           Buscar
         </Link>
         <Link
           href="/dashboard/albums"
-          className="text-base font-semibold transition-colors hover:text-primary"
+          className={cn(
+            "text-base font-semibold transition-colors hover:text-primary",
+            {
+              "text-primary": pathname === "/dashboard/albums",
+            }
+          )}
         >
           Mis albumes
         </Link>
@@ -35,10 +53,16 @@ export function AuthHeader() {
         <Button
           onClick={handleLogout}
           variant="ghost"
-          className="text-base font-semibold hover:text-primary transition-colors flex items-center p-0 h-auto"
+          className="text-base font-semibold hover:text-primary transition-colors flex items-center !p-0 h-auto"
         >
-          Cerrar sesión
+          <LogOut className="inline lg:hidden" />
+          <span className="hidden lg:inline">Cerrar sesión</span>
         </Button>
+        <Separator
+          orientation="vertical"
+          className="!h-6 bg-foreground inline md:hidden"
+        />
+        <ThemeModeToggle className="inline md:hidden" />
       </div>
     </header>
   );
--- src/app/(not-auth)/_layout/not-auth-header.tsx
+++ src/app/(not-auth)/_layout/not-auth-header.tsx
@@ -1,6 +1,6 @@
 export default function NotAuthHeader() {
   return (
-    <header className="font-montserrat w-full h-20 px-20 py-6 flex-col justify-center items-start gap-2.5">
+    <header className="font-montserrat w-full h-20 px-6 lg:px-20 py-6 flex-col justify-center items-start gap-2.5">
       <div className="h-6 uppercase">Album Spotify</div>
     </header>
   );
